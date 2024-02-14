0bc2ae36fcef8f400777fe4b5c5e80eeeddff71c
VexyPT <danifilipe221205@gmail.com>
2024-02-14 19:09:16 +0000

    jobs command

--- src/SlashCommands/economy/jobs.js
+++ src/SlashCommands/economy/jobs.js
@@ -1,4 +1,4 @@
-const { ApplicationCommandType, ActionRowBuilder, StringSelectMenuBuilder } = require("discord.js");
+const { ApplicationCommandType, ActionRowBuilder, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = {
   name: "jobs",
@@ -14,28 +14,39 @@ module.exports = {
 
     const jobs = {
         garbage_collector: {
+            name: "Garbage Collector",
             emoji: "🗑️",
             cooldown: 1000 * 60 * 45,
+            description: "Collect trash from the streets",
             maxMoney:  800
         },
         fisherman: {
+            name: "Fisherman",
             emoji: "🎣",
             cooldown: 1000 * 60 * 540,
+            description: "Go fish with your dad (if you have one)",
             maxMoney: 1600
         },
         sedex_delivery: {
+            name: "Sedex Delivery",
             emoji: "📦",
             cooldown: 1000 * 60 * 420,
+            description: "Deliver packages",
             maxMoney: 2000
         },
         truck_driver: {
+            name: "Truck Driver",
             emoji: "🚚",
             cooldown: 1000 * 60 * 300,
+            description: "Drive and drive for hours to deliver some chicken feed to companies",
             maxMoney: 2500
         },
         game_developer: {
+            name: "Game Developer",
             emoji: "👾",
-            cooldown: 1000 * 60 * 600
+            cooldown: 1000 * 60 * 600,
+            description: "Make games with GDScript, or bugs, you choose",
+            maxMoney: 7000
         }
     };
 
@@ -56,8 +67,44 @@ module.exports = {
 
     await interaction.reply({
         content: `> Choose a job`,
-        components: [row]
-    });
+        components: [row], ephemeral: true, fetchReply: true
+    }).then(msg => {
+        const collector = msg.createMessageComponentCollector({ idle: 1000 * 60 * 1});
+
+        collector.on("collect", async i => {
+            if (i.isStringSelectMenu()) {
+
+                const convertCooldownToTimestamp = (milliseconds) => {
+                    const seconds = Math.floor(milliseconds / 1000);
+                    return `<t:${seconds}:R>`;
+                };
+                
+                const embedJob = new EmbedBuilder()
+                .setTitle(`${jobs[work].name} ${jobs[work].emoji}`)
+                .addFields(
+                    { name: `> **Description:**`, value: `\`${jobs[work].description}\``},
+                    { name: `> **Cooldown:**`, value: `\`${convertCooldownToTimestamp(jobs[work].cooldown)}\``},
+                    { name: `> **Max Salary:**`, value: `${jobs[work].maxMoney + 500}`}
+                )
+
+                const work = i.values[0];
+
+                const button = new ActionRowBuilder().addComponents(
+                    new ButtonBuilder()
+                    .setCustomId(`accept_${work}`)
+                    .setEmoji(jobs[work].emoji)
+                    .setLabel("Accept Job")
+                    .setStyle(ButtonStyle.Secondary)
+                );                
+                
+                i.update({
+                    content: ``,
+                    embeds: [embedJob],
+                    components: [button]
+                });
+            }
+        })
+    })
 
   }
 }
