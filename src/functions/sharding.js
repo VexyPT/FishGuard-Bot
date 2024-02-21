db622dbee4cd64203ec5127011515096b2e50ca4
VexyPT <danifilipe221205@gmail.com>
2024-02-21 11:45:19 +0000

    starting to setup sharding system (not necessary for now)

--- src/functions/sharding.js
+++ src/functions/sharding.js
@@ -1,7 +1,24 @@
-require('dotenv').config();
+const dotenv = require("dotenv");
+const fs = require("fs");
 const { ShardingManager } = require('discord.js');
 
-const manager = new ShardingManager('../../index.js', {
+let tokenFile = ".env";
+if (fs.existsSync('.env.development')) {
+  tokenFile = '.env.development';
+}
+
+// Loads the environment variables from the corresponding .env file
+const result = dotenv.config({
+  path: tokenFile
+});
+
+// Checks if there was an error loading the environment variables
+if (result.error) {
+  console.error("Error loading environment variables:", result.error);
+  process.exit(1);
+}
+
+const manager = new ShardingManager("../../index.js", {
 	token: process.env.clientToken,
 	totalShards: 'auto',
 	respawn: true,
