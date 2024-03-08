e328f7854c55208de58b516123e7a6441f590397
VexyPT <danifilipe221205@gmail.com>
2024-03-08 13:01:32 +0000

    Add: Userinfo Command

--- src/discord/commands/public/userSubCommand.ts
+++ src/discord/commands/public/userSubCommand.ts
@@ -1,7 +1,7 @@
 import { Command } from "#base";
 import {
     ApplicationCommandType, ApplicationCommandOptionType, EmbedBuilder,
-    ButtonBuilder, ButtonStyle, formatEmoji
+    ButtonBuilder, ButtonStyle, formatEmoji, codeBlock, time
 } from "discord.js";
 import { settings } from "#settings";
 import { createRow, hexToRgb } from "@magicyan/discord";
@@ -27,7 +27,7 @@ new Command({
                 {
                     name: "user",
                     nameLocalizations: {
-                        "pt-BR": "usuario"
+                        "pt-BR": "user"
                     },
                     description: "Mention or ID",
                     descriptionLocalizations: {
@@ -40,16 +40,16 @@ new Command({
         },
         {
             name: "banner",
-            description: "See a banner from a user",
+            description: "「Discord」 See a banner from a user",
             descriptionLocalizations: {
-                "pt-BR": "Veja um banner de um usuário do servidor"
+                "pt-BR": "「Discord」 Veja um banner de um usuário do servidor"
             },
             type: ApplicationCommandOptionType.Subcommand,
             options: [
                 {
                     name: "user",
                     nameLocalizations: {
-                        "pt-BR": "usuário"
+                        "pt-BR": "user"
                     },
                     description: "Mention or ID",
                     descriptionLocalizations: {
@@ -62,16 +62,16 @@ new Command({
         },
         {
             name: "info",
-            description: "Show a user's information",
+            description: "「Discord」 Show a user's information",
             descriptionLocalizations: {
-                "pt-BR": "Mostre as informações de um usuário no Discord"
+                "pt-BR": "「Discord」 Mostre as informações de um usuário no Discord"
             },
             type: ApplicationCommandOptionType.Subcommand,
             options: [
                 {
                     name: "user",
                     nameLocalizations: {
-                        "pt-BR": "usuário"
+                        "pt-BR": "user"
                     },
                     description: "Mention or ID",
                     descriptionLocalizations: {
@@ -168,6 +168,92 @@ new Command({
                 });
                 break;
             } // fim do /user banner
+
+            case "info": {
+
+                const user = options.getUser("user") || interaction.user;
+
+                const flags = user.flags?.toArray();
+                const flag2 = {
+                    "Staff": `${formatEmoji(settings.emojis.static.badges.staff)}`,
+                    "Partner": `${formatEmoji(settings.emojis.static.badges.partner)}`,
+                    "CertifiedModerator": `${formatEmoji(settings.emojis.static.badges.moderator)}`,
+                    "Hypesquad": "",
+                    "HypeSquadOnlineHouse1": "",
+                    "HypeSquadOnlineHouse2": "",
+                    "HypeSquadOnlineHouse3": "",
+                    "BugHunterLevel1": `${formatEmoji(settings.emojis.static.badges.bugHunter)}`,
+                    "BugHunterLevel2": `${formatEmoji(settings.emojis.static.badges.goldBugHunter)}`,
+                    "ActiveDeveloper": "",
+                    "VerifiedDeveloper": `${formatEmoji(settings.emojis.static.badges.earlyDeveloper)}`,
+                    "PremiumEarlySupporter": `${formatEmoji(settings.emojis.static.badges.earlySupporter)}`,
+                    "VerifiedBot": "",
+                };
+
+                let badge: string[] = [];
+
+                if (flags) {
+                    badge = flags
+                        .filter(flag => flag in flag2)
+                        .map(flag => flag2[flag as keyof typeof flag2]);
+                }
+
+                const badges = badge.join("");
+
+                axios.get(`https://discord.com/api/users/${user.id}`, {
+                    headers: {
+                        Authorization: `Bot ${client.token}`,
+                    },
+                }).then((res) => {
+
+                    const { banner } = res.data;
+                    const createdAt = user.createdAt;
+
+                    const embedInfo = new EmbedBuilder({
+                        thumbnail: { url: user.displayAvatarURL({ size: 1024 })},
+                        color: hexToRgb(settings.colors.azoxo)
+                    });
+
+                    if (badges?.length! > 255) {
+                        embedInfo.setAuthor({ name: `${user.globalName}`, iconURL: `${user.displayAvatarURL()}` });
+                        embedInfo.addFields(
+                            { name: `${formatEmoji(settings.emojis.static.dot)} Insígnias do Discord`, value: `${badges}`, inline: false },
+                        );
+                    } else {
+                        embedInfo.setTitle(`${user.globalName} ${badges}`);
+                    }
+
+                    embedInfo.addFields(
+                        { name: `${formatEmoji(settings.emojis.static.user)} Nome`, value: codeBlock(`${user.username}`), inline: true },
+                        { name: `${formatEmoji(settings.emojis.static.info)} Identidade`, value: codeBlock(`${user.id}`), inline: true },
+                        { name: `${formatEmoji(settings.emojis.static.mention)} Menção`, value: `${user}`, inline: true },
+                        { name: `${formatEmoji(settings.emojis.static.calendar)} Criou a conta em:`, value: `${time(createdAt!, "f")} (${time(createdAt!, "R")})`, inline: false }
+                    );
+
+                    if (banner) {
+                        const extension = banner.startsWith("a_") ? ".gif?size=4096" : ".png?size=4096";
+                        const url = `https://cdn.discordapp.com/banners/${user.id}/${banner}${extension}`;
+                        embedInfo.setImage(url);
+                    }
+
+                    const member = guild.members.cache.get(user.id);
+                    if (member) {
+
+                        const joinedAt = member.joinedAt;
+
+                        embedInfo.addFields(
+                            { name: `${formatEmoji(settings.emojis.static.plus)} Entrou no servidor em:`, value: `${time(joinedAt!, "f")} (${time(joinedAt!, "R")})`, inline: false }
+                        );
+                    }
+
+                    interaction.reply({
+                        embeds: [embedInfo]
+                    });
+
+                });
+
+                break;
+            }
         }
 
     }
